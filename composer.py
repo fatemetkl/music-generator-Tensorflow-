c8f49a80b42116693a80fb501f63ccc0704a8b26
Amirreza Amouie <aamouie01@gmail.com>
2019-01-24 13:50:40 +0330

    make MIDIProcessor class

--- composer.py
+++ composer.py
@@ -1,31 +1,8 @@
 
-import pretty_midi 
+import pretty_midi
 import numpy as np
+from midiProcessor import MIDIProcessor
 
+midiprocessor = MIDIProcessor(1)
 
-
-for i in (1,1):
-
-    song=pretty_midi.PrettyMIDI("data/{}.mid".format(i))
-    
-    max_instrument_note_length = 0
-    for instrument in song.instruments:
-        if len(instrument.notes)>max_instrument_note_length:
-            max_instrument_note_length = len(instrument.notes)
-    for instrument in song.instrument:
-        if len(instrument.notes)==max_instrument_note_length:
-            maxVel=0.0
-            maxPitch=0.0
-            maxDuration=0.0   
-            for note in instrument.notes:
-                    duration=note.end - note.start
-                    if(duration>maxDuration):
-                        maxDuration=duration
-                    if(note.velocity>maxVel):
-                        maxVel=note.velocity
-                    if(note.pitch>maxPitch):
-                        maxPitch=note.pitch        
-            break
-
-
-    final_list.append([song,maxVel,maxPitch,maxDuration])
+midiprocessor.read_all_files()
--- midiProcessor.py
+++ midiProcessor.py
@@ -1,41 +1,57 @@
-import pretty_midi 
+import pretty_midi
 import numpy as np
 
-final_list=np.array(1,4)
 
-# def lable_encoder(n):
-    
-#     binDuration=duration% np.argmax(final_list,axis=)
-#     binV=n.velocity%np.argmax(final_list,axis=3)
-    
-    
+class MIDIProcessor:
 
+    def __init__(self, size):
+        self.final_list = np.array(0)
+        self.size = size
 
+    def read_all_files(self):
+        for i in (1, self.size):
+            song = pretty_midi.PrettyMIDI("data/{}.mid".format(i))
+            maxVel, maxPitch, maxDuration = 0.0, 0.0, 0.0
+            max_instrument_note_length = 0
+            for instrument in song.instruments:
+                if len(instrument.notes) > max_instrument_note_length:
+                    max_instrument_note_length = len(instrument.notes)
+            for instrument in song.instruments:
+                if len(instrument.notes) == max_instrument_note_length:
+                    maxVel, maxPitch, maxDuration = 0.0, 0.0, 0.0
+                    for note in instrument.notes:
+                        duration = note.end - note.start
+                        if (duration > maxDuration):
+                            maxDuration = duration
+                        if (note.velocity > maxVel):
+                            maxVel = note.velocity
+                        if (note.pitch > maxPitch):
+                            maxPitch = note.pitch
+                    break
+            np.append(self.final_list, [song, maxVel, maxPitch, maxDuration])
 
+    def encode_song(self, song):
+        midi_data = pretty_midi.PrettyMIDI(song)
+        encoded_song = []
+        max_instrument_note_length = 0
+        for instrument in song.instruments:
+            if len(instrument.notes) > max_instrument_note_length:
+                max_instrument_note_length = len(instrument.notes)
+        for instrument in midi_data.instruments:
+            if (instrument.name == "Drum Kit 2"):
+                continue
+            if not instrument.is_drum:
+                if len(instrument.notes) == max_instrument_note_length:  # choose the most effective instrument
+                    for n in instrument.notes:
+                        encoded_song.append(n)
+                    break
 
+        return encoded_song
 
-def encode_song(song):
-    midi_data=pretty_midi.PrettyMIDI(song)
-    encoded_song=[]
-    max_instrument_note_length = 0
-    for instrument in song.instruments:
-        if len(instrument.notes)>max_instrument_note_length:
-            max_instrument_note_length = len(instrument.notes)
-    for instrument in midi_data.instruments:
-        if(instrument.name=="Drum Kit 2"):
-            continue
-        if not instrument.is_drum:
-            if len(instrument.notes)==max_instrument_note_length :  #choose the most effective instrument
-                for n in instrument.notes:
-                    encoded_song.append(n)
-                break
+    def write(self, song):
+        pass
 
-
-    return encoded_song            
-
-
-
-
-                  
-
-
+    def label_encoder(n):
+        pass
+        # binDuration=duration% np.argmax(final_list,axis=)
+        # binV=n.velocity%np.argmax(final_list,axis=3)
