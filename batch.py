8b9176292df986019752a5041ebe35094c82b74f
Amirreza Amouie <aamouie01@gmail.com>
2019-01-25 17:00:41 +0330

    write get_batch function

--- batch.py
+++ batch.py
@@ -10,4 +10,8 @@ class BatchProcessor:
         self.has_leftover_batch = False if self.max_iteration_num == int(self.total_size / batch_size) else True
 
     def get_batch(self, iteration):
-        
+        batch = []
+        for item in range(iteration * self.batch_size, (iteration + 1) * self.batch_size):
+            batch.append(self.data[item])
+
+        return batch
